7e099f73c230f2a391a0401876ea53130687abb6
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-08-04 04:17:31 +0000

    layout

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,14 +7,22 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    if @cart_item.customer_id = current_customer.id
+    @cart_item = CartItem.find_by(customer_id: current_customer.id, item_id: params[:cart_item][:item_id])
+    if @cart_item
+      @cart_item.amount += params[:cart_item][:amount].to_i
       @cart_item.save
-      flash[:success] = "カートに追加しました。"
+      flash[:success] = "カートに存在済のアイテムです"
       redirect_to cart_items_path
     else
-      flash[:danger] = "予期せぬエラーが発生しました"
-      redirect_to cart_items_path
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.save
+        flash[:success] = "カートに追加しました"
+        redirect_to cart_items_path
+      else
+        flash[:danger] = "予期せぬエラーが発生しました"
+        redirect_back(fallback_location: root_path)
+      end
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  
+  has_many :cart_items
+  has_many :orders
+  has_many :order_details
+  
   has_one_attached :image
   
   def get_image
