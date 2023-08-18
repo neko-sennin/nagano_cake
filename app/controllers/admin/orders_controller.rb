e6e8768bc1ea0085678f99eb881d11495cc4a793
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-08-18 18:17:46 +0000

    layout

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,25 @@
 class Admin::OrdersController < ApplicationController
   
   def index
-   
+    @order = Order.all
   end
   
   def show
+    @order = Order.find(params[:id])
+    @orders = Order.all
+    @order_details = OrderDetail.where(order_id: @order)
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order)
+  end
+  
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :total_price)
   end
   
 end
