f6354a4739e1933d6510e9f350904e379427918f
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-08-19 07:19:51 +0000

    Admin layout

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,7 +13,17 @@ class Admin::CustomersController < ApplicationController
   end
   
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    flash[:notice] = "会員情報を変更しました。"
+    redirect_to admin_customer_path
+  end
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email )
   end
     
-  
 end
