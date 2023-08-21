f04a5511b1d51fdc134464afbdcd8b84e0c6f05d
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-08-21 08:52:24 +0000

    layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,10 @@
 class Public::HomesController < ApplicationController
   
   def top
-    @items = Item.all.order(created_at: :desc)
+    @items = Item.all.order(created_at: :desc).limit(4)
   end
   
-  def abou
+  def about
   end
     
   
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,11 +2,11 @@ class Public::ItemsController < ApplicationController
   
   def index
     @items = Item.page(params[:page])
+    @items_all = Item.all
   end
 
   def show
     @item = Item.find(params[:id])
-    @items = @item.items.page(params[:page])
     @cart_item = CartItem.new
   end
   
